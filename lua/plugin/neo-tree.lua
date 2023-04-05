d57944833c39f1f661dfa4c40b985feedafe769f
Louie Lee <louielee.dev@gmail.com>
2023-04-05 16:38:33 +0800

    ref(neo-tree): remove redundant command

--- lua/plugin/neo-tree.lua
+++ lua/plugin/neo-tree.lua
@@ -25,14 +25,15 @@ return {
     --   desc = "Explorer NeoTree (root dir)",
     -- },
     {
-      "<leader>fe",
+      "<leader>e",
+      -- "<leader>fe",
       function()
         require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
       end,
       desc = "Explorer NeoTree (cwd)",
     },
     -- { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
-    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+    -- { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
   },
   deactivate = function()
     vim.cmd([[Neotree close]])
