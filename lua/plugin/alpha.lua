e75d8cd1e236462c064472e940054955a4ceb0b5
Louie Lee <louielee.dev@gmail.com>
2023-04-07 23:06:16 +0800

    feat: add find hidden files telescope

--- lua/plugin/alpha.lua
+++ lua/plugin/alpha.lua
@@ -8,6 +8,7 @@ return {
     dashboard.section.header.val = vim.split(logo, "\n")
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("h", " " .. " Find hidden files", ":Telescope find_files hidden=true<CR>"),
       dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
       dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
