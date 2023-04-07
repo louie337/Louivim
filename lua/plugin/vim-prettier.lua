2e31b31a65bf73ad7a06ecccd71cdc5810767a80
Louie Lee <louielee.dev@gmail.com>
2023-04-07 18:20:29 +0800

    fix: Prettier bug

--- lua/plugin/vim-prettier.lua
+++ lua/plugin/vim-prettier.lua
@@ -1,4 +1,8 @@
 return {
   'prettier/vim-prettier',
-  build="yarn install --frozen-lockfile --production"
+  build = function()
+    os.execute("yarn install --frozen-lockfile --production")
+    vim.g.prettier.config.single_quote = true
+    vim.g.prettier.config.trailing_comma = 'all'
+  end
 }
