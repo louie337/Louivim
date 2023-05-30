2449eb7bdf374f43b7d3ba396b0dd0f43a18e4ca
Louie Lee <louie.lee@urbanspring.hk>
2023-05-30 16:23:36 +0800

    feat: update telescope command

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -44,9 +44,9 @@ return {
     -- end
     , { desc = '[/] Fuzzily search in current buffer' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sH', '<cmd>Telescope find_files hidden=true no_ignore=true<CR>', { desc = '[S]earch [H]idden files' })
-    vim.keymap.set('n', '<leader>sn', '<cmd>Telescope find_files hidden=true no_ignore=true<CR>', { desc = '[S]earch [N]o ignore' })
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sh', '<cmd>Telescope find_files hidden=true no_ignore=true<CR>', { desc = '[S]earch [H]idden files' })
+    -- vim.keymap.set('n', '<leader>sn', '<cmd>Telescope find_files hidden=true no_ignore=true<CR>', { desc = '[S]earch [N]o ignore' })
+    vim.keymap.set('n', '<leader>sH', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
