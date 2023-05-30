23c52700479c28461173d781499537f1703ef115
Louie Lee <louie.lee@urbanspring.hk>
2023-05-30 16:24:46 +0800

    feat: update packages & keymap

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -45,7 +45,7 @@ return {
     , { desc = '[/] Fuzzily search in current buffer' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sh', '<cmd>Telescope find_files hidden=true no_ignore=true<CR>', { desc = '[S]earch [H]idden files' })
-    -- vim.keymap.set('n', '<leader>sn', '<cmd>Telescope find_files hidden=true no_ignore=true<CR>', { desc = '[S]earch [N]o ignore' })
+    -- vim.keymap.set('n', '<leader>sn', '<cmd>T'elescope find_files hidden=true no_ignore=true<CR>', { desc = '[S]earch [N]o ignore' })
     vim.keymap.set('n', '<leader>sH', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
