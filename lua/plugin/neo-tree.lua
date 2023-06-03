016c8567c914d7a8973ccf14e413a7949c399058
Louie Lee <louielee.dev@gmail.com>
2023-06-03 18:03:23 +0800

    feat(neo-tree): update dependency

--- lua/plugin/neo-tree.lua
+++ lua/plugin/neo-tree.lua
@@ -8,6 +8,7 @@ return {
     {
       -- only needed if you want to use the commands with "_with_window_picker" suffix
       's1n7ax/nvim-window-picker',
+      tag = "v1.*",
       config = function()
         require 'window-picker'.setup({
           autoselect_one = true,
@@ -17,6 +18,7 @@ return {
             bo = {
               -- if the file type is one of following, the window will be ignored
               filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+
               -- if the buffer type is one of following, the window will be ignored
               buftype = { 'terminal', "quickfix" },
             },
