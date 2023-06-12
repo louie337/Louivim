131b92bc5edd74a66c9c3b0d972c207a527de012
Louie Lee <louie.lee@urbanspring.hk>
2023-06-12 16:23:30 +0800

    feat(lspconfig): update fidget version

--- lua/plugin/nvim-lspconfig.lua
+++ lua/plugin/nvim-lspconfig.lua
@@ -1,13 +1,16 @@
-return { -- LSP Configuration & Plugins
+return {
+         -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
-    'williamboman/mason.nvim', -- Automatically install LSPs to stdpath for neovim
-    'williamboman/mason-lspconfig.nvim', -- Useful status updates for LSP
-    -- NOTE `opts = {}` is the same as calling `require('fidget').setup({})`
-    {
-        'j-hui/fidget.nvim',
-        opts = {}
-    }, -- Additional lua configuration, makes nvim stuff amazing!
-    'folke/neodev.nvim'
+        'williamboman/mason.nvim',       -- Automatically install LSPs to stdpath for neovim
+        'williamboman/mason-lspconfig.nvim', -- Useful status updates for LSP
+        -- NOTE `opts = {}` is the same as calling `require('fidget').setup({})`
+        {
+            'j-hui/fidget.nvim',
+            opts = {
+                version = 'legacy'
+            }
+        }, -- Additional lua configuration, makes nvim stuff amazing!
+        'folke/neodev.nvim'
     }
 }
