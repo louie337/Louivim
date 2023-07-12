5e7ed43eaba6e4f51843599bed60db17cb9502fa
Louie Lee <louie.lee@urbanspring.hk>
2023-07-12 12:36:16 +0800

    feat(telescope): add no ignore in find files to allow searching .gitignored files

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -48,7 +48,7 @@ return {
     -- end
     , { desc = '[/] Fuzzily search in current buffer' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sh', '<CMD>Telescope find_files hidden=true<CR>',
+    vim.keymap.set('n', '<leader>sh', '<CMD>Telescope find_files hidden=true no_ignore=true<CR>',
     { desc = '[S]earch [H]idden files' })
     vim.keymap.set('n', '<leader>sH', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
