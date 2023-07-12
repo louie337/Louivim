6ace5a7e6ad708a397e3248003c3470f63b68946
Louie Lee <louie.lee@urbanspring.hk>
2023-07-12 11:11:02 +0800

    feat(telescope): add fuzzy grep

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -48,10 +48,11 @@ return {
     -- end
     , { desc = '[/] Fuzzily search in current buffer' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sh', '<cmd>Telescope find_files hidden=true<CR>',
+    vim.keymap.set('n', '<leader>sh', '<CMD>Telescope find_files hidden=true<CR>',
     { desc = '[S]earch [H]idden files' })
     vim.keymap.set('n', '<leader>sH', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sG', '<CMD>Telescope grep_string search=<CR>', { desc = '[S]earch by [G]rep (Fuzzy)' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>ss', builtin.spell_suggest, { desc = '[S]earch [S]pell Suggestions' })
