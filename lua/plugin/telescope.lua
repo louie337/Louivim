b3f310927cf2f3fbda2d17ccecef9562e7f87749
Louie Lee <louielee.dev@gmail.com>
2023-12-04 10:06:05 +0800

    feat: update telescope jumplist feature

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -31,11 +31,12 @@ return {
           "node_modules",
           "^.git/",
           ".next"
-        }
+        },
       },
     }
     -- NOTE: Keymap settings
     local builtin = require('telescope.builtin')
+    local utils = require('telescope.utils')
     -- See `:help telescope.builtin`
     vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
     vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find
@@ -49,14 +50,19 @@ return {
     , { desc = '[/] Fuzzily search in current buffer' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sh', '<CMD>Telescope find_files hidden=true no_ignore=true<CR>',
-    { desc = '[S]earch [H]idden files' })
+      { desc = '[S]earch [H]idden files' })
     vim.keymap.set('n', '<leader>sH', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sG', '<CMD>Telescope grep_string search=<CR>', { desc = '[S]earch by [G]rep (Fuzzy)' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>ss', builtin.spell_suggest, { desc = '[S]earch [S]pell Suggestions' })
-    vim.keymap.set('n', '<leader>sj', builtin.jumplist, { desc = '[S]earch [J]ump list' })
+    vim.keymap.set('n', '<leader>sJ', builtin.jumplist, { desc = '[S]earch [J]ump list' })
+    vim.keymap.set('n', '<leader>sj', function()
+      print(require("telescope.utils").buffer_dir())
+      builtin.jumplist({ initial_mode = "normal", default_text = utils.buffer_dir() })
+    end
+    , { desc = '[S]earch [J]ump list in current directory' })
     vim.keymap.set('n', '<leader>st', builtin.tagstack, { desc = '[S]earch [T]agstack' })
     vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
     vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
