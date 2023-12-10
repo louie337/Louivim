2689731fe3e79384f518d7cd5c2f5182b9b14fe1
Louie Lee <louielee.dev@gmail.com>
2023-12-10 09:39:25 +0800

    feat: update base config

--- lua/config/base/init.lua
+++ lua/config/base/init.lua
@@ -7,6 +7,7 @@ vim.g.markdown_recommended_style = 0
 -- List & Map style settings
 local opt = vim.opt
 
+-- opt.cmdheight = 0 -- Make the line invisible but also will disturb workflow
 opt.mouse = "a"
 opt.number = true
 opt.relativenumber = true
@@ -14,20 +15,20 @@ opt.scrolloff = 4
 opt.sidescrolloff = 8
 -- NOTE: No need to share clipboard since I am using builtin system clipboard access
 -- opt.clipboard = "unnamedplus"
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
-opt.cursorline = true -- Enable highlighting of the current line
-opt.ignorecase = true -- Ignore case
-opt.smartcase = true -- Don't ignore case with capitals
+opt.confirm = true     -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true  -- Enable highlighting of the current line
+opt.ignorecase = true  -- Ignore case
+opt.smartcase = true   -- Don't ignore case with capitals
 opt.smartindent = true -- Insert indents automatically
 opt.spell = true
 opt.spelllang = { "en" }
 opt.spelloptions = { "camel" }
-opt.splitbelow = false -- Put new windows below current
-opt.splitright = true -- Put new windows right of current
-opt.tabstop = 2 -- Number of spaces tabs count for
-opt.shiftwidth = 2 -- Fix shiftwidth 8 issue
-opt.termguicolors = true -- True color support
-opt.hlsearch = false -- Remove highlight search on search
+opt.splitbelow = false   -- Put new windows below current
+opt.splitright = true    -- Put new windows right of current
+opt.tabstop = 2          -- Number of spaces tabs count for
+opt.shiftwidth = 2       -- Fix shiftwidth 8 issue
+opt.termguicolors = true -- True color support & override iterm color settings
+opt.hlsearch = false     -- Remove highlight search on search
 -- NOTE: No need to modify since I am using plugin to manage the fold
 -- opt.foldenable = false
 -- opt.foldmethod = 'indent'
