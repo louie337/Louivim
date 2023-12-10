7e5c5fc365c34bcc9d444d9674128a4bf653134f
Louie Lee <louielee.dev@gmail.com>
2023-12-10 09:40:46 +0800

    feat: show number line in symbol outline

--- lua/plugin/symbols-outline.lua
+++ lua/plugin/symbols-outline.lua
@@ -5,6 +5,8 @@ return {
     keys = { { "<leader>so", "<cmd>SymbolsOutline<cr>", desc = "[S]ymbols [O]utline" } },
     opts = {
       position = "left",
+      show_relative_numbers=true,
+      show_number=true,
     },
   },
 }
