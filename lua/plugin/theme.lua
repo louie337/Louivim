4069fc54aab535dde5e5e5f27338fb75adf16a5f
Louie Lee <louielee.dev@gmail.com>
2023-12-10 09:39:36 +0800

    feat: update theme fallback

--- lua/plugin/theme.lua
+++ lua/plugin/theme.lua
@@ -25,8 +25,8 @@ return {
       functions = {},
       variables = {},
       -- Background styles. Can be "dark", "transparent" or "normal"
-      sidebars = "dark",            -- style for sidebars, see below
-      floats = "dark",              -- style for floating windows
+      sidebars = "storm",            -- style for sidebars, see below
+      floats = "storm",              -- style for floating windows
     },
     sidebars = { "qf", "help" },    -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
     day_brightness = 0.3,           -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
