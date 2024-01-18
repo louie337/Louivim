7f33a86b1d9b6ffa11f7dac1995e496cf19fabec
Louie Lee <louielee.dev@gmail.com>
2024-01-18 12:10:39 +0800

    feat(none-ls): remove spelling

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,6 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.completion.spell,
       },
     })
   end,
