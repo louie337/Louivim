f8504f0798efe552141abb1b9a42571105a8c011
Louie Lee <louielee.dev@gmail.com>
2024-01-18 10:30:14 +0800

    ref: remove ale

--- lua/plugins/ale.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "dense-analysis/ale",
-  config = function()
-    vim.keymap.set("n", "<leader>pp", "<cmd>:ALEFix<CR>", { desc = "ALEFix" })
-    vim.g.ale_fixers = { 'prettier', 'eslint' }
-  end,
-}
