c4c84d7627bfcd04b4569e7be927408c8be166b8
Louie Lee <louielee.dev@gmail.com>
2024-01-18 10:33:45 +0800

    ref: indentation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,18 @@
-return { {
-  -- NOTES: LSP Server Manager
-  "williamboman/mason.nvim",
-}, {
-  -- NOTES: Useful status updates for LSP
-  'williamboman/mason-lspconfig.nvim',
-  dependencies = {
-    'j-hui/fidget.nvim',
-    'folke/neodev.nvim'
+return {
+  {
+    -- NOTES: LSP Server Manager
+    "williamboman/mason.nvim",
   },
-}, {
-  -- NOTE: LSP Configuration & Plugins
-  'neovim/nvim-lspconfig',
-} }
+  {
+    -- NOTES: Useful status updates for LSP
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = {
+      'j-hui/fidget.nvim',
+      'folke/neodev.nvim'
+    },
+  },
+  {
+    -- NOTE: LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+  }
+}
