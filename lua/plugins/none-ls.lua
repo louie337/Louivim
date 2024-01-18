19cccfbc1f5487bcc6dac3b48a7dc1543cfb918e
Louie Lee <louielee.dev@gmail.com>
2024-01-18 13:52:47 +0800

    feat: update prettier settings

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,12 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettierd.with({
+          condition = function(utils)
+            return utils.root_has_file({ ".prettierrc" })
+          end,
+          prefer_local = "node_modules/.bin",
+        }),
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.code_actions.eslint,
       },
