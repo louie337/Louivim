c3c12baf43d9b97a517da977eebd597a9bdda123
Louie Lee <louielee.dev@gmail.com>
2024-01-20 16:04:30 +0800

    ref(todo-comments): remove unnecessary alternatives

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -9,23 +9,19 @@ return {
       FIX = {
         icon = " ",
         color = "error",
-        alt = { "IMPORTANT", "Important", "important", "BUG" },
+        alt = { "IMPORTANT", "BUG" },
       },
-      TODO = { icon = " ", color = "info", alt = { "Todo", "todo" } },
+      TODO = { icon = " ", color = "info" },
       WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-      PERF = { icon = " ", alt = { "COMMENT", "Comment", "comment" } },
-      NOTE = { icon = " ", color = "hint", alt = { "INFO", "Note", "note" } },
-      TEST = { icon = " ", color = "test", alt = { "QUESTION", "Question", "question" } },
+      PERF = { icon = " ", alt = { "COMMENT" } },
+      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      TEST = { icon = " ", color = "test", alt = { "QUESTION" } },
     },
     highlight = {
       multiline = false,
-      pattern = [[.*<(KEYWORDS)(S|s|\s|:)]], -- pattern or table of patterns, used for highlighting (vim regex)
     },
-    search = {
-      pattern = [[\b(KEYWORDS)(S\s|\s|:)]], -- ripgrep regex
-    }
   },
   keys = {
     { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "[S]earch [T]odo" },
-  }
+  },
 }
