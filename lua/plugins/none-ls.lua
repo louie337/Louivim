9d3b6151c2cb864c317fe4ba8ecee4694ab1532e
Louie Lee <louielee.dev@gmail.com>
2024-01-20 15:28:09 +0800

    feat(none-ls): update prettier & eslint settings

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,16 +5,38 @@ return {
 
     null_ls.setup({
       sources = {
+        -- NOTES: Lua settings
         null_ls.builtins.formatting.stylua,
+
+        -- NOTES: Prettier settings
         null_ls.builtins.formatting.prettierd.with({
           condition = function(utils)
-            return utils.root_has_file({ ".prettierrc" })
+            return utils.root_has_file_matches("prettierrc")
           end,
           prefer_local = "node_modules/.bin",
           extra_filetypes = { "mdx" },
         }),
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.code_actions.eslint_d,
+
+        -- NOTES: Eslnit settings
+        null_ls.builtins.diagnostics.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file_matches("eslintrc")
+          end,
+          prefer_local = "node_modules/.bin",
+        }),
+        null_ls.builtins.code_actions.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file_matches("eslintrc")
+          end,
+          prefer_local = "node_modules/.bin",
+        }),
+
+        -- NOTES: Cspell settings
+        null_ls.builtins.diagnostics.cspell.with({
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity = vim.diagnostic.severity.HINT
+          end,
+        }),
       },
     })
   end,
