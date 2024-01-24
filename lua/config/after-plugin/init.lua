ecabfd3d3c39ae931ce82c6f0f5e23d534ea5cd9
Louie Lee <louielee.dev@gmail.com>
2024-01-24 12:08:27 +0800

    feat: add friendly-snippets

--- lua/config/after-plugin/init.lua
+++ lua/config/after-plugin/init.lua
@@ -89,7 +89,7 @@ capabilities.textDocument.foldingRange = {
 }
 
 -- Ensure the servers above are installed
-local mason= require("mason")
+local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 local lspconfig = require("lspconfig")
 
@@ -119,6 +119,7 @@ local cmp = require("cmp")
 local luasnip = require("luasnip")
 
 luasnip.config.setup({})
+require("luasnip.loaders.from_vscode").lazy_load()
 
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
@@ -128,12 +129,15 @@ cmp.setup({
             luasnip.lsp_expand(args.body)
         end,
     },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
     mapping = cmp.mapping.preset.insert({
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-u>"] = cmp.mapping.scroll_docs(4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete({}),
         ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         }),
         ["<Tab>"] = cmp.mapping(function(fallback)
@@ -155,11 +159,10 @@ cmp.setup({
             end
         end, { "i", "s" }),
     }),
-    sources = { {
-        name = "nvim_lsp",
-    }, {
-        name = "luasnip",
-    } },
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    },
 })
 
 -- treesitter
