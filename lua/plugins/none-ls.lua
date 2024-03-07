70d3d3765b26f15021869f4f6a4e0cebc8ea3e45
Louie Lee <louielee.dev@gmail.com>
2024-03-07 11:31:44 +0800

    fix(none-ls): breaking changes

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
 
@@ -9,27 +12,27 @@ return {
         null_ls.builtins.formatting.stylua,
 
         -- NOTES: Prettier settings
-        null_ls.builtins.formatting.prettierd.with({
-          prefer_local = "node_modules/.bin",
+        null_ls.builtins.formatting.prettier.with({
+          only_local = "node_modules/.bin",
           extra_filetypes = { "mdx" },
         }),
 
         -- NOTES: Eslnit settings
-        null_ls.builtins.diagnostics.eslint_d.with({
+        require("none-ls.diagnostics.eslint_d").with({
           condition = function(utils)
             return utils.root_has_file_matches("eslintrc")
           end,
-          prefer_local = "node_modules/.bin",
+          only_local = "node_modules/.bin",
         }),
-        null_ls.builtins.code_actions.eslint_d.with({
+        require("none-ls.code_actions.eslint_d").with({
           condition = function(utils)
             return utils.root_has_file_matches("eslintrc")
           end,
-          prefer_local = "node_modules/.bin",
+          only_local = "node_modules/.bin",
         }),
 
         -- NOTES: Cspell settings
-        null_ls.builtins.diagnostics.cspell.with({
+        null_ls.builtins.completion.spell.with({
           diagnostics_postprocess = function(diagnostic)
             diagnostic.severity = vim.diagnostic.severity.HINT
           end,
