2f91a38aa5ccc9d5e99afce83d5042057a2eaec3
louie <louie.lee@datax.io>
2024-04-02 11:15:07 +0800

    feat: update ignore patterns

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,7 +31,8 @@ return {
         file_ignore_patterns = {
           ".node_modules/",
           ".git/",
-          ".next/"
+          ".next/",
+          ".lock"
         },
       },
     })
