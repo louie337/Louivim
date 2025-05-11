35d7f620cfac22f501322f880847a3118eab9f62
Louie Lee <louielee.dev@gmail.com>
2025-05-11 18:16:50 +0800

    feat: update help splitting orientation

--- lua/config/base/init.lua
+++ lua/config/base/init.lua
@@ -68,3 +68,8 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "help", "man" },
+    command = "wincmd H", -- Move the help window to far left
+})
