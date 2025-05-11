1ab711b856bef84e494aa6fc2a007fae95b6b8fb
Louie Lee <louielee.dev@gmail.com>
2025-05-11 18:19:27 +0800

    feat: add lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,19 +1,21 @@
+local telescope = require("telescope")
 return {
   {
     -- NOTES: LSP Server Manager
     "williamboman/mason.nvim",
-    config=true,
+    config = true,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
   },
   {
     -- NOTE: LSP Configuration & Plugins
     "neovim/nvim-lspconfig",
-    config = function()
-    end,
     keys = {
-      -- { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition" },
-      -- { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References", nowait = true },
-      -- { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
-      -- { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
+      { "gd", function() telescope.lsp_definitions({ reuse_win = true }) end,      desc = "[G]oto [D]efinition" },
+      { "gr", function() telescope.lsp_references({}) end,                         desc = "[G]oto [R]eferences" },
+      { "gi", function() telescope.lsp_implementations({ reuse_win = true }) end,  desc = "[G]oto [I]mplementation" },
+      { "gt", function() telescope.lsp_type_definitions({ reuse_win = true }) end, desc = "[G]oto [T]ype Definition" },
     }
   }
 }
