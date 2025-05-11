63c29fd54fabab8a295673ba7930b18c0331af48
Louie Lee <louielee.dev@gmail.com>
2025-05-11 21:58:29 +0800

    feat: update noice & update lualine-x items

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,7 @@ return {
   event = "VeryLazy",
   opts = function(plugin)
     local icons = require("options.icons")
+    local noice = require("noice")
 
     local function fg(name)
       return function()
@@ -42,17 +43,24 @@ return {
           -- stylua: ignore
         },
         lualine_x = {
-          -- stylua: ignore
           {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = fg("Statement")
+            noice.api.status.message.get_hl,
+            cond = noice.api.status.message.has,
           },
-          -- stylua: ignore
           {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = fg("Constant"),
+            noice.api.status.command.get,
+            cond = noice.api.status.command.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            noice.api.status.mode.get,
+            cond = noice.api.status.mode.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            noice.api.status.search.get,
+            cond = noice.api.status.search.has,
+            color = { fg = "#ff9e64" },
           },
           {
             require("lazy.status").updates,
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -16,9 +16,6 @@ return {
         lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
       },
     },
-    messages = {
-      enabled = false,
-    },
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
@@ -29,11 +26,11 @@ return {
     },
     -- you can enable a preset for easier configuration
     presets = {
-      bottom_search = false,     -- use a classic bottom cmdline for search
-      command_palette = false,   -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false,        -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false,    -- add a border to hover docs and signature help
+      bottom_search = false,      -- use a classic bottom cmdline for search
+      command_palette = false,    -- position the cmdline and popupmenu together
+      long_message_to_split = false, -- long messages will be sent to a split
+      inc_rename = false,         -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false,     -- add a border to hover docs and signature help
     },
   },
   dependencies = {
