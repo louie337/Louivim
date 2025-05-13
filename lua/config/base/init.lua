1c1f1bdf4d1e675bc5051cd8adab82f5764004be
Louie Lee <louielee.dev@gmail.com>
2025-05-14 00:03:47 +0800

    fix: remove deprecating commands

--- lua/config/base/init.lua
+++ lua/config/base/init.lua
@@ -40,7 +40,7 @@ end
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
   group = highlight_group,
   pattern = "*",
