d804152605f6cc1090b49e1bb8d8a7f85e81e260
Louie Lee <louielee.dev@gmail.com>
2025-05-14 22:36:30 +0800

    feat: update lsp, remove unused plugins & use nvim 0.11.1

---
+++ lua/plugins/snacks/init.lua
@@ -0,0 +1,24 @@
+local dashboard = require("plugins.snacks.dashboard")
+
+return {
+  "folke/snacks.nvim",
+  dependencies = {},
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    image = { enabled = false },
+    dashboard = dashboard,
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = false },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    lazygit = { enabled = true },
+  },
+}
