6c71f5196d7d650b560ac22262ea7c21a9786753
Louie Lee <louielee.dev@gmail.com>
2025-05-17 12:14:49 +0800

    feat: update cmp (testing sorting)

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,43 +1,50 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "folke/lazydev.nvim", "hrsh7th/nvim-cmp" },
-  opts = {
-    keymap = {
-      preset = "default",
-      ["<C-space>"] = {},
-      ["<C-n>"] = { "select_next", "show", "fallback_to_mappings" },
-    },
+	"saghen/blink.cmp",
+	dependencies = {
+		"folke/lazydev.nvim",
+		"hrsh7th/nvim-cmp",
+		"Kaiser-Yang/blink-cmp-avante",
+		"rafamadriz/friendly-snippets",
+	},
+	opts = {
+		keymap = {
+			preset = "default",
+			["<C-space>"] = {},
+			["<C-n>"] = { "show", "select_next" },
+		},
 
-    completion = {
-      keyword = { range = "full" },
-      accept = { auto_brackets = { enabled = false } },
-      menu = {
-        auto_show = true,
-        draw = {
-          columns = {
-            { "label",     "label_description", gap = 4 },
-            { "kind_icon", "kind" },
-          },
-        },
-      },
-      documentation = { auto_show = true },
-      ghost_text = { enabled = true },
-    },
+		completion = {
+			keyword = { range = "full" },
+			accept = { auto_brackets = { enabled = false } },
+			menu = {
+				auto_show = true,
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 4 },
+						{ "kind_icon", "kind" },
+					},
+				},
+			},
+			documentation = { auto_show = true },
+			ghost_text = { enabled = true },
+		},
 
-    signature = { enabled = true },
+		fuzzy = { implementation = "lua" },
 
-    fuzzy = { implementation = "lua" },
-  },
-
-  sources = {
-    default = { "lsp", "path", "buffer", "snippets" },
-    providers = {
-      lsp = {
-        score_offset = 1000,
-      },
-      snippets = {
-        score_offset = -100,
-      },
-    },
-  },
+		sources = {
+			default = { "avante", "lsp", "path", "buffer", "snippets" },
+			providers = {
+				lsp = {
+					score_offset = 10,
+				},
+				snippets = {
+					score_offset = 0,
+				},
+				avante = {
+					module = "blink-cmp-avante",
+					name = "Avante",
+				},
+			},
+		},
+	},
 }
