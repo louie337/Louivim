e4ffa4763bfdc2b95380c9bdd01a6fc23761c28b
Louie Lee <louie.lee@datax.io>
2025-07-02 11:13:00 +0800

    ref: remove friendly snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,36 +1,36 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "folke/lazydev.nvim", "hrsh7th/nvim-cmp", "rafamadriz/friendly-snippets" },
-  opts = {
-    keymap = {
-      preset = "default",
-      ["<C-space>"] = {},
-      ["<C-n>"] = { "select_next", "show", "fallback_to_mappings" },
-    },
+	"saghen/blink.cmp",
+	dependencies = { "folke/lazydev.nvim", "hrsh7th/nvim-cmp" },
+	opts = {
+		keymap = {
+			preset = "default",
+			["<C-space>"] = {},
+			["<C-n>"] = { "select_next", "show", "fallback_to_mappings" },
+		},
 
-    completion = {
-      keyword = { range = "full" },
-      accept = { auto_brackets = { enabled = false } },
-      menu = {
-        auto_show = true,
-        draw = {
-          columns = {
-            { "label",     "label_description", gap = 4 },
-            { "kind_icon", "kind" },
-          },
-        },
-      },
-      documentation = { auto_show = true },
-      ghost_text = { enabled = true },
-    },
+		completion = {
+			keyword = { range = "full" },
+			accept = { auto_brackets = { enabled = false } },
+			menu = {
+				auto_show = true,
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 4 },
+						{ "kind_icon", "kind" },
+					},
+				},
+			},
+			documentation = { auto_show = true },
+			ghost_text = { enabled = true },
+		},
 
-    signature = { enabled = true },
+		signature = { enabled = true },
 
-    fuzzy = { implementation = "lua" },
-  },
+		fuzzy = { implementation = "lua" },
+	},
 
-  sources = {
-    default = { "lsp", "path", "buffer", "lazydev", "friendly-snippets", "snippets" },
-    min_keyword_length = 0,
-  },
+	sources = {
+		default = { "lsp", "path", "buffer", "lazydev", "snippets" },
+		min_keyword_length = 0,
+	},
 }
